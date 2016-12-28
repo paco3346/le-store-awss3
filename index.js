98f3f9d61ece71c6a37e165bc0a3117e6cbf7a83
Michael Ambrose <paco3346@gmail.com>
2016-12-28 10:50:35 -0500

    Initial commit of an S3 implementation

--- index.js
+++ index.js
@@ -1,126 +1,191 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const PromiseA = require('bluebird');
+
 module.exports.create = function (options) {
-
-
-
-  var defaults = {};
-
-
-
+  const s3 = PromiseA.promisifyAll(new AWS.S3({params: {Bucket: options.bucket}}));
+  var log = function() {
+    if (options.debug) {
+      var args = Array.prototype.slice.call(arguments);
+      console.log.apply(console, args);
+    }
+  };
   var accounts = {
 
     // Accounts
-    setKeypair: function (opts, keypair, cb) {
-      // opts.email     // optional
-      // opts.accountId // optional - same as returned from acounts.set(opts, reg)
-
-
-      // SAVE to db (as PEM and/or JWK) and index each domain in domains to this keypair
-      // keypair = { privateKeyPem: '...', privateKeyJwk: { ... } }
-      cb(null, keypair);
-    }
+    setKeypairAsync: function (opts, keypair) {
+      log('setting account keypair for', opts.email);
+      return s3.putObjectAsync({
+        Key: `accounts/${opts.email}/private_key.json`,
+        Body: JSON.stringify(keypair.privateKeyJwk)
+      }).then(function() {
+        return keypair;
+      }).catch(function(e) {
+        console.error(e);
+      });
+    },
     // Accounts
-  , checkKeypair: function (opts, cb) {
-      // opts.email // optional
-      // opts.accountId // optional - same as returned from acounts.set(opts, reg)
-
-
-      // check db and return null or keypair object with one
-      // (or both) of privateKeyPem or privateKeyJwk
-      cb(null, { privateKeyPem: '...', privateKeyJwk: {} });
-    }
-
-
+    checkKeypairAsync: function (opts) {
+      log('checking account keypair for', opts.email);
+      return s3.getObjectAsync({
+        Key: `accounts/${opts.email}/private_key.json`
+      }).then(function(obj) {
+        log('account keypair found');
+        return {privateKeyJwk: JSON.parse(obj.Body.toString())};
+      }).catch(function(e) {
+        if (e.statusCode == 404) {
+          log('account keypair not found');
+          return null;
+        } else {
+          console.error(e);
+        }
+      });
+    },
 
     // Accounts
-  , check: function (opts, cb) {
-      // opts.email       // optional
-      // opts.accountId   // optional - same as returned from acounts.set(opts, reg)
-      // opts.domains     // optional - same as set in certificates.set(opts, certs)
-
-      // return account from db if it exists, otherwise null
-      cb(null, { id: '...', keypair: { privateKeyJwk: {} }/*, domains: []*/ });
-    }
+    checkAsync: function (opts) {
+      log('checking account', opts.email);
+      return s3.getObjectAsync({
+        Key: `accounts/${opts.email}/private_key.json`
+      }).then(function(obj) {
+        log('account found');
+        return {id: opts.email, keypair: { privateKeyJwk: JSON.parse(obj.Body.toString())}}
+      }).catch(function(e) {
+        if (e.statusCode == 404) {
+          log('account', opts.email, 'not found');
+          return null;
+        } else {
+          console.error(e);
+        }
+      });
+    },
     // Accounts
-  , set: function (opts, reg, cb) {
-      // opts.email
-      // reg.keypair
-      // reg.receipt // response from acme server
-
-
-      // You must implement a method to deterministically generate 'id'
-      // For example, you could do this:
-      // var id = crypto.createHash('sha256').update(reg.keypair.publicKeyPem).digest('hex');
-      cb(null, { id: '...', email: opts.email, keypair: reg.keypair, receipt: reg.receipt });
+    setAsync: function (opts, reg) {
+      log('setting account', opts.email);
+      return s3.putObjectAsync({
+        Key: `accounts/${opts.email}/private_key.json`,
+        Body: JSON.stringify(reg.keypair.privateKeyJwk)
+      }).then(function() {
+        return {id: opts.email, email: opts.email, keypair: reg.keypair, receipt: reg.receipt};
+      }).catch(function(e) {
+        console.error(e);
+      });
     }
-
   };
-
-
 
   var certificates = {
 
     // Certificates
-    setKeypair: function (opts, keypair, cb) {
-      // opts.domains - this is an array, but you nly need the first (or any) of them
+    setKeypairAsync: function (opts, keypair) {
+      log('setting cert keypair', opts.domains);
 
+      var promises = [];
+      for (var i=0; i < opts.domains.length; i++) {
+        promises.push(s3.putObjectAsync({
+          Key: `certs/${opts.domains[i]}/domain_key.json`,
+          Body: JSON.stringify(keypair.privateKeyJwk)
+        }));
+      }
 
-      // SAVE to db (as PEM and/or JWK) and index each domain in domains to this keypair
-      cb(null, keypair);
-    }
+      return PromiseA.all(promises)
+        .then(function() {
+          return keypair;
+        }).catch(function(e) {
+          console.error(e);
+        });
+    },
     // Certificates
-  , checkKeypair: function (opts, cb) {
-      // opts.domains - this is an array, but you only need the first (or any) of them
+    checkKeypairAsync: function (opts) {
+      log('checking cert keypair', opts.domains);
 
+      var promises = [];
+      for (var i=0; i < opts.domains.length; i++) {
+        promises.push(s3.getObjectAsync({
+          Key: `certs/${opts.domains[i]}/domain_key.json`
+        }));
+      }
 
-      // check db and return null or keypair object with one of privateKeyPem or privateKeyJwk
-      cb(null, { privateKeyPem: '...', privateKeyJwk: {} });
-    }
-
-
+      return PromiseA.all(promises)
+        .then(function(arr) {
+          log('cert keypair found');
+          //in theory the keypairs should be the same for all domains on the same cert
+          //if they are return the shared key, if not return null which will rewrite all the keys to a single shared key
+          var areEqual = function() {
+            var len = arguments.length;
+            for (var i = 1; i< len; i++){
+              if (arguments[i].Body.toString() !== arguments[i-1].Body.toString())
+                return false;
+            }
+            return true;
+          };
+          //if they all match we can just return the first one (since we can only return 1)
+          return  {privateKeyJwk: areEqual(arr) ? JSON.parse(arr[0].Body.toString()) : null}
+        }).catch(function(e) {
+          if (e.statusCode == 404) {
+            log('cert keypair not found');
+            return null;
+          } else {
+            console.error(e);
+          }
+        });
+    },
 
     // Certificates
-  , check: function (opts, cb) {
-      // You will be provided one of these (which should be tried in this order)
-      // opts.domains
-      // opts.email // optional
-      // opts.accountId // optional
+    checkAsync: function (opts) {
+      log('looking for cert for', opts.domains, 'under account', opts.email);
 
+      var promises = [];
+      for (var i=0; i < opts.domains.length; i++) {
+        promises.push(s3.getObjectAsync({Key: `certs/${opts.domains[i]}/cert.pem`}));
+        promises.push(s3.getObjectAsync({Key: `certs/${opts.domains[i]}/privkey.pem`}));
+        promises.push(s3.getObjectAsync({Key: `certs/${opts.domains[i]}/chain.pem`}));
+      }
 
-      // return certificate PEMs from db if they exist, otherwise null
-      // optionally include expiresAt and issuedAt, if they are known exactly
-      // (otherwise they will be read from the cert itself later)
-      cb(null, { privkey: 'PEM', cert: 'PEM', chain: 'PEM', domains: [], accountId: '...' });
-    }
+      return PromiseA.all(promises)
+        .then(function(arr) {
+          return {
+            cert: arr[0].Body.toString(),
+            privkey: arr[1].Body.toString(),
+            chain: arr[2].Body.toString()
+          }
+        }).catch(function(e) {
+          if (e.statusCode == 404) {
+            log('cert not found');
+            return null;
+          } else {
+            console.error(e);
+          }
+        });
+    },
     // Certificates
-  , set: function (opts, pems, cb) {
-      // opts.domains   // each of these must be indexed
-      // opts.email     // optional, should be indexed
-      // opts.accountId // optional - same as set by you in accounts.set(opts, keypair) above
+    setAsync: function (opts) {
+      log('setting cert');
 
-      // pems.privkey
-      // pems.cert
-      // pems.chain
+      var promises = [];
+      for (var i=0; i < opts.domains.length; i++) {
+        promises.push(s3.putObjectAsync({Key: `certs/${opts.domains[i]}/cert.pem`, Body: opts.certs.cert}));
+        promises.push(s3.putObjectAsync({Key: `certs/${opts.domains[i]}/privkey.pem`, Body: opts.certs.privkey}));
+        promises.push(s3.putObjectAsync({Key: `certs/${opts.domains[i]}/chain.pem`, Body: opts.certs.chain}));
+      }
 
-
-      // SAVE to the database, index the email address, the accountId, and alias the domains
-      cb(null, pems);
+      return PromiseA.all(promises)
+        .then(function() {
+          //log(opts.certs);
+          return opts.certs;
+        }).catch(function(e) {
+          console.error(e);
+        });
     }
-
   };
-
-
 
   return {
     getOptions: function () {
       // merge options with default settings and then return them
       return options;
-    }
-  , accounts: accounts
-  , certificates: certificates
+    },
+    accounts: accounts,
+    certificates: certificates
   };
-
-
 
 };
